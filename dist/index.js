c8d2765720d80c78a29a0b87b364a010baac4530
Kyle <rhodestraveler@gmail.com>
2018-01-25 16:11:17 -0600

    failed yarn run pre-deploy

--- dist/index.js
+++ dist/index.js
@@ -12,10 +12,6 @@ var _mongoose = require("mongoose");
 
 var _mongoose2 = _interopRequireDefault(_mongoose);
 
-var _passport = require("passport");
-
-var _passport2 = _interopRequireDefault(_passport);
-
 var _OrderRoutes = require("./routes/OrderRoutes");
 
 var _OrderRoutes2 = _interopRequireDefault(_OrderRoutes);
@@ -29,6 +25,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // dotenv allows us to declare environment variables in a .env file, \
 // find out more here https://github.com/motdotla/dotenv
 require("dotenv").config();
+// import passport from "passport";
 
 
 _mongoose2.default.set("debug", true);
@@ -36,14 +33,17 @@ _mongoose2.default.Promise = global.Promise;
 _mongoose2.default.connect("mongodb://rhodestraveler:rhodestraveler@ds143907.mlab.com:43907/aca-final-project");
 
 var app = (0, _express2.default)();
-app.use(_bodyParser2.default.json());
 app.use(_express2.default.static("public"));
+
+app.get("/", function (req, res, next) {
+  res.sendFile("public/index.html");
+});
+app.use(_bodyParser2.default.json());
 app.use(_OrderRoutes2.default);
 app.use(_ProductRoutes2.default);
 
-app.get("*", (req, res, next) => {
-  res.sendFile("public/index.html");
-});
+// const authStrategy = passport.authenticate("authStrategy", { session: false });
+// app.use(authStrategy);
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
